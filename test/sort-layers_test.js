7f9b18fb17a03120b7b48c57967692c884a473c4
metacollin <metacollin@electropi.mp>
2023-06-13 12:44:03 -0600

    initial commit

--- test/sort-layers_test.js
+++ test/sort-layers_test.js
@@ -2,117 +2,111 @@
 'use strict'
 
 var expect = require('chai').expect
+var extend = require('xtend')
 
+var wtg = require('whats-that-gerber')
 var sortLayers = require('../lib/sort-layers')
 
-describe('sort layers function', function () {
-  it('should reduce layers into an object with keys top and bottom', function () {
+function mockLayer(side, type, converter) {
+  return {
+    side: side,
+    type: type,
+    converter: extend(converter, {layer: ['foo']}),
+  }
+}
+
+describe('sort layers function', function() {
+  it('should reduce layers into object keyed by layer type', function() {
     var result = sortLayers([])
 
     expect(result.top).to.eql([])
     expect(result.bottom).to.eql([])
     expect(result.drills).to.eql([])
-    expect(result.outline).to.be.null
+    expect(result.outline).to.equal(null)
   })
 
-  it('should add top layers to the top array', function () {
+  it('should add top layers to the top array', function() {
     var layers = [
-      {type: 'tcu', converter: {}},
-      {type: 'tsm', converter: {}},
-      {type: 'tss', converter: {}},
-      {type: 'tsp', converter: {}}
+      mockLayer(wtg.SIDE_TOP, wtg.TYPE_COPPER, {}),
+      mockLayer(wtg.SIDE_TOP, wtg.TYPE_SOLDERMASK, {}),
+      mockLayer(wtg.SIDE_TOP, wtg.TYPE_SILKSCREEN, {}),
+      mockLayer(wtg.SIDE_TOP, wtg.TYPE_SOLDERPASTE, {}),
     ]
 
     var result = sortLayers(layers)
 
-    expect(result.bottom).to.be.empty
-    expect(result.drill).to.be.empty
-    expect(result.outline).to.be.null
-    expect(result.top).to.eql([
-      {type: 'cu', converter: layers[0].converter},
-      {type: 'sm', converter: layers[1].converter},
-      {type: 'ss', converter: layers[2].converter},
-      {type: 'sp', converter: layers[3].converter}
-    ])
+    expect(result.bottom).to.have.lengthOf(0)
+    expect(result.drills).to.have.lengthOf(0)
+    expect(result.outline).to.equal(null)
+    expect(result.top).to.eql(layers)
   })
 
-  it('should add bottom layers to the bottom array', function () {
+  it('should add bottom layers to the bottom array', function() {
     var layers = [
-      {type: 'bcu', converter: {}},
-      {type: 'bsm', converter: {}},
-      {type: 'bss', converter: {}},
-      {type: 'bsp', converter: {}}
+      mockLayer(wtg.SIDE_BOTTOM, wtg.TYPE_COPPER, {}),
+      mockLayer(wtg.SIDE_BOTTOM, wtg.TYPE_SOLDERMASK, {}),
+      mockLayer(wtg.SIDE_BOTTOM, wtg.TYPE_SILKSCREEN, {}),
+      mockLayer(wtg.SIDE_BOTTOM, wtg.TYPE_SOLDERPASTE, {}),
     ]
 
     var result = sortLayers(layers)
 
-    expect(result.top).to.be.empty
-    expect(result.drills).to.be.empty
-    expect(result.outline).to.be.null
-    expect(result.bottom).to.eql([
-      {type: 'cu', converter: layers[0].converter},
-      {type: 'sm', converter: layers[1].converter},
-      {type: 'ss', converter: layers[2].converter},
-      {type: 'sp', converter: layers[3].converter}
-    ])
+    expect(result.top).to.have.lengthOf(0)
+    expect(result.drills).to.have.lengthOf(0)
+    expect(result.outline).to.equal(null)
+    expect(result.bottom).to.eql(layers)
   })
 
-  it('should add mechanical layers to the mech array', function () {
+  it('should add mechanical layers to the mech array', function() {
     var layers = [
-      {type: 'out', converter: {}},
-      {type: 'drl', converter: {defs: 'drl1'}},
-      {type: 'drl', converter: {defs: 'drl2'}}
+      mockLayer(wtg.SIDE_ALL, wtg.TYPE_OUTLINE, {}),
+      mockLayer(wtg.SIDE_ALL, wtg.TYPE_DRILL, {defs: 'drl1'}),
+      mockLayer(wtg.SIDE_ALL, wtg.TYPE_DRILL, {defs: 'drl2'}),
     ]
 
     var result = sortLayers(layers)
 
-    expect(result.top).to.be.empty
-    expect(result.bottom).to.be.empty
-    expect(result.outline).to.eql({type: 'out', converter: layers[0].converter})
-    expect(result.drills).to.eql([
-      {type: 'drl', converter: layers[1].converter},
-      {type: 'drl', converter: layers[2].converter}
-    ])
+    expect(result.top).to.have.lengthOf(0)
+    expect(result.bottom).to.have.lengthOf(0)
+    expect(result.outline).to.eql(layers[0])
+    expect(result.drills).to.eql([layers[1], layers[2]])
   })
 
-  it('should ignore everything else', function () {
+  it('should ignore invalid or incomplete layers', function() {
     var layers = [
-      {type: 'drw', converter: {}},
-      {type: 'drw', converter: {}},
-      {type: 'drw', converter: {}}
+      {type: wtg.TYPE_DRAWING, converter: {layer: ['foo']}},
+      {side: wtg.SIDE_TOP, type: wtg.TYPE_COPPER},
+      {side: wtg.SIDE_TOP, type: wtg.TYPE_DRAWING, converter: {layer: []}},
+      {side: wtg.SIDE_TOP, type: wtg.TYPE_COPPER},
+      {type: null, converter: {}},
+      null,
     ]
 
     var result = sortLayers(layers)
 
-    expect(result.top).to.be.empty
-    expect(result.bottom).to.be.empty
-    expect(result.drills).to.be.empty
-    expect(result.outline).to.be.null
+    expect(result.top).to.have.lengthOf(0)
+    expect(result.bottom).to.have.lengthOf(0)
+    expect(result.drills).to.have.lengthOf(0)
+    expect(result.outline).to.equal(null)
   })
 
-  it('should include the externalId field of layers', function () {
+  it('should include the externalId field of layers', function() {
     var layers = [
-      {type: 'tcu', externalId: 'foo', converter: {foo: 'foo'}},
-      {type: 'bsm', externalId: 'bar', converter: {bar: 'bar'}},
-      {type: 'drl', externalId: 'baz', converter: {baz: 'baz'}},
-      {type: 'out', externalId: 'quux', converter: {quux: 'quux'}}
+      extend(mockLayer(wtg.SIDE_TOP, wtg.TYPE_COPPER, {}), {externalId: 'foo'}),
+      extend(mockLayer(wtg.SIDE_BOTTOM, wtg.TYPE_SOLDERMASK, {}), {
+        externalId: 'bar',
+      }),
+      extend(mockLayer(wtg.SIDE_ALL, wtg.TYPE_DRILL, {}), {externalId: 'baz'}),
+      extend(mockLayer(wtg.SIDE_ALL, wtg.TYPE_OUTLINE, {}), {
+        externalId: 'quux',
+      }),
     ]
 
     var result = sortLayers(layers)
 
-    expect(result.top).to.eql([
-      {type: 'cu', externalId: 'foo', converter: layers[0].converter}
-    ])
-    expect(result.bottom).to.eql([
-      {type: 'sm', externalId: 'bar', converter: layers[1].converter}
-    ])
-    expect(result.drills).to.eql([
-      {type: 'drl', externalId: 'baz', converter: layers[2].converter}
-    ])
-    expect(result.outline).to.eql({
-      type: 'out',
-      externalId: 'quux',
-      converter: layers[3].converter
-    })
+    expect(result.top).to.eql([layers[0]])
+    expect(result.bottom).to.eql([layers[1]])
+    expect(result.drills).to.eql([layers[2]])
+    expect(result.outline).to.eql(layers[3])
   })
 })
--- test/wrap-layer_test.js
+++ test/wrap-layer_test.js
@@ -1,18 +1,14 @@
 // test suite for the wrap layer function
 'use strict'
 
-var chai = require('chai')
+var expect = require('chai').expect
 var sinon = require('sinon')
-var sinonChai = require('sinon-chai')
 var xmlElementString = require('xml-element-string')
-var expect = chai.expect
-
-chai.use(sinonChai)
 
 var wrapLayer = require('../lib/wrap-layer')
 
-describe('wrap layer function', function () {
-  it('should return the layer value wrapped in a group with an id', function () {
+describe('wrap layer function', function() {
+  it('should return the layer value wrapped in a group with an id', function() {
     var layer = {layer: ['SOME_STUFF']}
     var element = sinon.spy(xmlElementString)
     var result = wrapLayer(element, 'id', layer)
@@ -22,7 +18,7 @@ describe('wrap layer function', function () {
     expect(result).to.equal(element.returnValues[0])
   })
 
-  it('should be able to scale the layer', function () {
+  it('should be able to scale the layer', function() {
     var layer = {layer: ['SOME_STUFF']}
     var element = sinon.spy(xmlElementString)
     var result = wrapLayer(element, 'foobar', layer, 25.4)
@@ -32,7 +28,7 @@ describe('wrap layer function', function () {
     expect(result).to.equal(element.returnValues[0])
   })
 
-  it('should not add a transformation if the scale is 1', function () {
+  it('should not add a transformation if the scale is 1', function() {
     var layer = {layer: ['SOME_STUFF']}
     var element = sinon.spy(xmlElementString)
     var result = wrapLayer(element, 'id', layer, 1)
@@ -42,7 +38,7 @@ describe('wrap layer function', function () {
     expect(result).to.equal(element.returnValues[0])
   })
 
-  it('should be able to use a tag other than <g>', function () {
+  it('should be able to use a tag other than <g>', function() {
     var layer = {layer: ['SOME_STUFF']}
     var element = sinon.spy(xmlElementString)
     var result = wrapLayer(element, 'id', layer, 1, 'clipPath')
